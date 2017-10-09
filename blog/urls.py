4efc6d6feccadfd65995d0f0f8569c7a4870a81b
tianshixu <tonyxu0124@yahoo.com>
2017-10-09 21:58:36 +0800

    Add markdown functions.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
 
 app_name = 'blog'
-urlpatterns = [url(r'^$', views.index, name='index'),
+urlpatterns = [
+               url(r'^admin/', admin.site.urls),
+               url(r'^$', views.index, name='index'),
                url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
                ]
