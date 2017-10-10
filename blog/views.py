3d91b887b72093981e0fd44a8bfc6b326912ba33
tianshixu <tonyxu0124@yahoo.com>
2017-10-10 21:21:33 +0800

    Add category

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 import markdown
 # Create your views here.
 
@@ -21,10 +21,16 @@ def detail(request, pk):
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
 
-#/archieves/2017/3/
+
+# /archives/2017/3/
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month
                                     ).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list':post_list})
