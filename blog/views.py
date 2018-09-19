b23b6f5b111da413a3b2cbacbb9151a534c31ae2
tianshixu <tonyxu0124@yahoo.com>
2018-09-19 23:26:27 +0800

    New functions

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
-from .models import Post, Category
+from .models import Post, Category, Tag
 import markdown
 from comments.form import CommentForm
 from django.views.generic import ListView, DetailView
@@ -83,6 +83,79 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+
+        left = []
+        right = []
+
+        left_has_more = False
+        right_has_more = False
+
+        first = False
+        last = False
+
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        if page_number == 1:
+            right = page_range[page_number: page_number+2]
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                last = True
+
+        elif page_number == total_pages:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+
+        else:
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+            data = {
+                'left': left,
+                'right': right,
+                'left_has_more': left_has_more,
+                'right_has_more': right_has_more,
+                'first': first,
+                'last': last,
+            }
+
+            return data
+
 
 class CategoryView(ListView):
     model = Post
@@ -92,6 +165,7 @@ class CategoryView(ListView):
     def get_queryset(self):
         cate = get_object_or_404(Category, pk = self.kwargs.get('pk'))
         return super(Category, self).get_queryset().filter(category=cate)
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -134,7 +208,6 @@ class ArchivesView(ListView):
         return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
                                                                created_time__month=month
                                                                )
-
 class CategoryView(ListView):
     model = Post
     template_name = 'blog/index.html'
@@ -144,3 +217,12 @@ class CategoryView(ListView):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
 
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags = tag)
